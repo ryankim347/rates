052861f140e07c6fb728a53ca9335123409d514c
Ryan Kim <ryankim@ryans-mbp-14.mynetworksettings.com>
2024-01-27 18:31:43 -0500

    FINALLY

---
+++ my-app/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="d-flex mx-auto flex-col justify-center">
+        <div className='w-100 bg-black text-white p-5'> rizzyrates </div>
+         <div className="mx-auto d-flex p-10">
+          <input className='shadow-lg rounded-md border-black border-2 p-2 w-1/3' placeholder='Search'></input>
+         < /div>
+    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload. I sadfsadfhave rizzasdf
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  );
+}
+
+export default App;
