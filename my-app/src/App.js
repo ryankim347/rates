ea58abfad5954511bbef9672fdd9177f6f0a322d
Ryan Kim <ryankim@ryans-mbp-14.mynetworksettings.com>
2024-01-27 22:28:30 -0500

    server stuff

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,23 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
   return (
     <div className="d-flex mx-auto flex-col justify-center">
         <div className='w-100 bg-black text-white p-5'> rizzyrates </div>
          <div className="mx-auto d-flex p-10">
           <input className='shadow-lg rounded-md border-black border-2 p-2 w-1/3' placeholder='Search'></input>
-         < /div>
+          <p>{!data ? "Loading..." : data}</p>
+         </div>
     </div>
     // <div className="App">
     //   <header className="App-header">
@@ -15,7 +25,7 @@ function App() {
     //     <p>
     //       Edit <code>src/App.js</code> and save to reload. I sadfsadfhave rizzasdf
     //     </p>
-    //     <a
+    //     <an
     //       className="App-link"
     //       href="https://reactjs.org"
     //       target="_blank"
