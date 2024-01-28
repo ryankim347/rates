a26b5338a3e3eff5aa8ddd70547fad6fb6f78557
Ryan Kim <ryankim@ryans-mbp-14.mynetworksettings.com>
2024-01-27 22:37:56 -0500

    initial mongodb

--- my-app/server/index.js
+++ my-app/server/index.js
@@ -1,6 +1,21 @@
 // server/index.js
+require("dotenv").config();
 
 const express = require("express")
+const mongoose = require("mongoose");
+const mongoConnectionURL = process.env.MONGO_SRV;
+const databaseName = "restaurants";
+
+mongoose.set("strictQuery", false);
+// connect to mongodb
+mongoose
+  .connect(mongoConnectionURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: databaseName,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(`Error connecting to MongoDB: ${err}`));
 
 const PORT = process.env.PORT || 3001;
 
